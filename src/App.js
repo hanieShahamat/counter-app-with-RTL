94ea8d08cdbf48902a5e8d484e119ddfa9fcaa71
hanie <hanieshahamat@gmail.com>
2024-08-21 01:04:52 +0330

    TDD- counterapp with RTL

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="flex items-center justify-center py-12 my-12">
+        <i className="text-gray-300 px-7" class="ri-information-2-line"></i>
+        <i className="text-gray-300" class="ri-settings-2-line"></i>
+        <i
+          className="text-gray-300 px-7"
+          data-testid="reset"
+          onClick={() => setCount(0)}
+          class="ri-reset-left-fill"
+        ></i>
+      </div>
+      <div>
+        <h1
+          className="text-gray-300 flex items-center justify-center text-9xl font-bold"
+          data-testid="counter"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {count}
+        </h1>
+        <div className="text-gray-300 flex items-center justify-center py-12 mt-14">
+          <button
+            className="rounded-full outline outline-offset-0 outline-gray-300 p-2 px-4 mr-20"
+            data-testid="increase"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            +
+          </button>
+          <button
+            className="rounded-full outline outline-offset-0 outline-gray-300 p-2 px-5 ml-20"
+            data-testid="decrease"
+            onClick={() => setCount((count) => (count <= 0 ? 0 : count - 1))}
+          >
+            -
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, getByTestId, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Initial count value to be 0", () => {
+  render( <App/>)
+  const counterH1Element = screen.getByTestId("counter");
+  expect(counterH1Element).toHaveTextContent("0");
 });
+
+test("click button to increase counter", ()=>{
+  render( <App/>)
+  const increaseBtnnElement = screen.getByTestId("increase")
+  expect(increaseBtnnElement).toBeDefined()
+  fireEvent.click(increaseBtnnElement)
+  expect(screen.getByTestId("counter")).toHaveTextContent("1")
+})
+
+test("click button to decrease counter", ()=>{
+  render( <App/>)
+  const decreaseBtnnElement = screen.getByTestId("decrease")
+  expect(decreaseBtnnElement).toBeDefined()
+  fireEvent.click(decreaseBtnnElement)
+  expect(screen.getByTestId("counter")).toHaveTextContent("0")
+})
+
+test("click button to reset counter", ()=>{
+  render( <App/>)
+  const resetIconElement = screen.getByTestId("reset")
+  expect(resetIconElement).toBeDefined()
+  fireEvent.click(resetIconElement)
+  expect(screen.getByTestId("counter")).toHaveTextContent("0")
+})
